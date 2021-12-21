47a94edd386de32bb3de1297883cb83a73f952cf
JairoSolarteRodriguez <pvprandom578@gmail.com>
2021-12-20 19:01:35 -0500

    add form validate bugs fixed

--- js/app.js
+++ js/app.js
@@ -5,21 +5,90 @@ const btnMenu = document.getElementById('btn-menu'),
 function ocultar() {
     navegacion.classList.add('d-none');
     navegacion.classList.remove('dropdown-content');
+    if (btnMenu.classList.contains('btn-close')) {
+        btnMenu.classList.remove('btn-close');
+        btnMenu.classList.add('btn-menu');
+    }
+    if (btnMenu.classList.contains('btn-menu')) {
+        // btnMenu.classList.toggle('btn-menu');
+        btnMenu.classList.remove('btn-close');
+    }
+    // btnMenu.classList.contains('clase2')
 }
 
-// enciende y apaga las clases.
+// toggle class
 btnMenu.addEventListener('click', () => {
     navegacion.classList.toggle('dropdown-content');
     navegacion.classList.toggle('d-none');
+    btnMenu.classList.toggle('btn-close');
+    btnMenu.classList.toggle('btn-menu');
 });
 
-//funcion para cualquier clic en el documento
+//click in the doc
 document.addEventListener("click", function(e) {
-    /*obtiendo informacion del DOM para verificar si hacen click fuera
-    del documento para quitar los estilos y evitar errores.*/
+    /* get DOM info for click off check */
     let clic = e.target;
 
     if (clic != btnMenu) {
         ocultar();
     }
+});
+
+// Form Validate.
+
+const form = document.getElementById('form'),
+    alertName = document.getElementById('alert-name'),
+    alertEmail = document.getElementById('alert-email'),
+    btnSubmit = document.getElementById('btn-submit');
+
+const inputs = document.querySelectorAll('#form input');
+const expresions = {
+    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+const formValidate = (e) => {
+    switch (e.target.name) {
+        case 'name':
+            if (expresions.name.test(e.target.value)) {
+                document.getElementById('name').classList.remove('wrong');
+                alertName.classList.add('d-none');
+            } else {
+                document.getElementById('name').classList.remove('correct');
+                document.getElementById('name').classList.add('wrong');
+                alertName.classList.remove('d-none');
+                alertName.classList.add('alert');
+                alertName.innerText = `El nombre no puede contener síbolos ni números`;
+            }
+            if (e.target.value == '') {
+                alertName.classList.remove('d-none');
+                alertName.classList.add('alert');
+                alertName.innerText = `Por favor complete los campos`;
+            }
+            break;
+        case 'email':
+            if (expresions.email.test(e.target.value)) {
+                document.getElementById('email').classList.remove('wrong');
+                document.getElementById('email').classList.add('correct');
+                alertEmail.classList.add('d-none');
+            } else {
+                document.getElementById('email').classList.remove('correct');
+                document.getElementById('email').classList.add('wrong');
+                alertEmail.classList.remove('d-none');
+                alertEmail.classList.add('alert');
+                alertEmail.innerText = `Por favor ingrese un correo valido`;
+            }
+            if (e.target.value == '') {
+                alertEmail.classList.remove('d-none');
+                alertEmail.classList.add('alert');
+                alertEmail.innerText = `Por favor complete los campos`;
+            }
+            break;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('blur', formValidate);
+    input.addEventListener('keyup', formValidate);
+    input.addEventListener('focus', formValidate);
 });
